4423a28841c22d2f7b06fc13437892cf9add0b46
kunalshrivastavapune25 <kunalshrivastava.pune@gmail.com>
2026-02-04 20:21:48 +0530

    update

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "SAM Depoyment",
+            "message": "hello world",
             # "location": ip.text.replace("\n", "")
         }),
     }
